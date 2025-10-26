916133a3c233cd23847f2fd12738ef69d083e696
aperezg <alexisperezglez@gmail.com>
2025-10-26 09:10:04 +0100

    chore: initialize final petstore application with Spring Boot and Maven setup

---
+++ src/main/java/es/home/petstore/service/petstoreservice/FinalPetstoreApplication.java
@@ -0,0 +1,13 @@
+package es.home.petstore.service.petstoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalPetstoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinalPetstoreApplication.class, args);
+  }
+
+}
---
+++ src/test/java/es/home/petstore/service/petstoreservice/FinalPetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.home.petstore.service.petstoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalPetstoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
